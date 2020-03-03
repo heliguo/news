8083b41afb607c69e538c858443c03dd9f72e2a4
heliguo <ysulgha@163com>
2020-03-03 22:46:19 +0800

    splash页：动画 guide页：引导页，viewpager、指示器、view树 onLayout与onCreate方法的关系 视图树观察者

---
+++ app/src/main/java/com/lgh/news/GuideActivity.java
@@ -0,0 +1,143 @@
+package com.lgh.news;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+public class GuideActivity extends BaseActivity {
+
+    private LinearLayout linearLayout;
+    private Button button;
+    private ViewPager viewPager;
+    private ArrayList<ImageView> imageViews = new ArrayList<>();
+    private int[] mIamgedIds = {R.drawable.bg_android, R.drawable.bg_ios, R.drawable.bg_js};
+
+    private float pointDis;
+
+    private ImageView redImageView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏在setContentView之前
+        setContentView(R.layout.activity_guide);
+        viewPager = findViewById(R.id.guide_viewpager);
+        linearLayout = findViewById(R.id.guide_ll_container);
+        redImageView = findViewById(R.id.guide_point_choose);
+        button = findViewById(R.id.guide_btn_start);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GuideActivity.this, HomeActivity.class));
+                //使用sharedPreferences 记录引导页访问记录，只加载一次
+                finish();
+            }
+        });
+        initData();
+    }
+
+    private void initData() {
+
+        for (int i = 0; i < mIamgedIds.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setBackgroundResource(mIamgedIds[i]);
+            imageViews.add(imageView);
+            ImageView point = new ImageView(this);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            if (i > 0) {
+                params.leftMargin = 10;
+            }
+            point.setLayoutParams(params);
+        }
+        viewPager.setAdapter(new GuideAdapter());
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                float distance = pointDis * positionOffset + pointDis * position;//加入当前位置信息
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) redImageView.
+                        getLayoutParams();
+                layoutParams.leftMargin = (int) distance;
+                redImageView.setLayoutParams(layoutParams);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == imageViews.size() - 1) {
+                    button.setVisibility(View.VISIBLE);
+                } else {
+                    button.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        /**
+         * linearLayout.getChildAt(1).getLeft()方法在 onLayout() 方法拿到，
+         * 而 onLayout() 等三个方法 在onCreate()方法后才能执行
+         *
+         * linearLayout.getChildAt(1).getLeft()=0；
+         */
+//        pointDis = linearLayout.getChildAt(1).getLeft() -
+//                linearLayout.getChildAt(0).getLeft();
+
+        /**
+         * 视图树观察者
+         * 监听全局layout
+         */
+        redImageView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            //layout方法布局完成
+            @Override
+            public void onGlobalLayout() {
+                pointDis = linearLayout.getChildAt(1).getLeft() -
+                        linearLayout.getChildAt(0).getLeft();
+                //移除观察者
+                redImageView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+
+    }
+
+    class GuideAdapter extends PagerAdapter {
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            container.addView(imageViews.get(position));
+            return imageViews.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mIamgedIds.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            container.removeView((View) object);
+        }
+    }
+}
---
+++ app/src/main/java/com/lgh/news/SplashActivity.java
@@ -0,0 +1,61 @@
+package com.lgh.news;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends BaseActivity {
+
+    private LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        linearLayout = findViewById(R.id.splash_ll_root);
+
+        RotateAnimation rotateAnimation = new RotateAnimation(0,360,
+                Animation.RELATIVE_TO_SELF,Animation.RELATIVE_TO_SELF);
+        rotateAnimation.setDuration(1000);
+        rotateAnimation.setFillAfter(true);
+
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0,1,0,1,
+                Animation.RELATIVE_TO_SELF,Animation.RELATIVE_TO_SELF);
+        scaleAnimation.setDuration(1000);
+        scaleAnimation.setFillAfter(true);
+
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0,1);
+        alphaAnimation.setDuration(2000);
+        alphaAnimation.setFillAfter(true);
+
+        AnimationSet set = new AnimationSet(false);
+        set.addAnimation(rotateAnimation);
+        set.addAnimation(scaleAnimation);
+        set.addAnimation(alphaAnimation);
+        linearLayout.startAnimation(set);
+        set.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(SplashActivity.this,GuideActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
