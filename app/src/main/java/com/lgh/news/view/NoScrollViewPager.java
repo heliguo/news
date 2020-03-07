99de956fdc8bcf9ea0af401bd908d8ed094b44a2
heliguo <ysulgha@163com>
2020-03-07 21:08:58 +0800

    view事件处理

--- app/src/main/java/com/lgh/news/view/NoScrollViewPager.java
+++ app/src/main/java/com/lgh/news/view/NoScrollViewPager.java
@@ -19,8 +19,42 @@ public class NoScrollViewPager extends ViewPager {
         super(context, attrs);
     }
 
+    /**
+     * 事件处理 dispatchTouchEvent -> onInterceptTouchEvent -> onTouchEvent
+     *
+     * @param ev
+     * @return
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        //请求父控件和所有祖先控件不要拦截事件
+//        getParent().requestDisallowInterceptTouchEvent(true);
+        return super.dispatchTouchEvent(ev);
+    }
+
+    /**
+     * 本身的拦截事件
+     *
+     * @param ev
+     * @return
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        //false 不拦截
+        return false;
+    }
+
+    /**
+     * 父类的onTouchEvent
+     *
+     * @param ev
+     * @return
+     */
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
+        //true不消耗事件
         return true;
     }
+
+
 }
