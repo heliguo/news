838722f6f151217653fffbefc4d6ed448487c400
heliguo <ysulgha@163com>
2020-03-08 15:43:28 +0800

    滑动事件处理

--- app/src/main/java/com/lgh/news/view/NoScrollViewPager.java
+++ app/src/main/java/com/lgh/news/view/NoScrollViewPager.java
@@ -10,6 +10,8 @@ import androidx.viewpager.widget.ViewPager;
 
 public class NoScrollViewPager extends ViewPager {
 
+    private int startX;
+    private int startY;
 
     public NoScrollViewPager(@NonNull Context context) {
         this(context, null);
@@ -21,6 +23,9 @@ public class NoScrollViewPager extends ViewPager {
 
     /**
      * 事件处理 dispatchTouchEvent -> onInterceptTouchEvent -> onTouchEvent
+     * 1、上下滑动，需要父控件拦截（父控件处理）
+     * 2、左滑：最后一个页面拦截
+     * 3、右滑：第一个页面拦截
      *
      * @param ev
      * @return
@@ -29,6 +34,37 @@ public class NoScrollViewPager extends ViewPager {
     public boolean dispatchTouchEvent(MotionEvent ev) {
         //请求父控件和所有祖先控件不要拦截事件
 //        getParent().requestDisallowInterceptTouchEvent(true);
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startX = (int) ev.getX();
+                startY = (int) ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int endX = (int) ev.getX();
+                int endY = (int) ev.getY();
+                int dx = endX - startX;
+                int dy = endY - startY;
+
+                if (Math.abs(dx) > Math.abs(dy)) {//左右滑动
+                    int currentItem = getCurrentItem();
+                    if (dx > 0) {//右滑
+                        if (currentItem == 0) {
+                            getParent().requestDisallowInterceptTouchEvent(false);
+                        }
+                    } else {//左滑
+                        //用adapter获得item总数
+                        if (currentItem == getAdapter().getCount()) {
+                            getParent().requestDisallowInterceptTouchEvent(true);
+                        }
+                    }
+                }else {//上下滑动
+                    getParent().requestDisallowInterceptTouchEvent(false);
+                }
+
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
         return super.dispatchTouchEvent(ev);
     }
 
